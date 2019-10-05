3b71a73892a3230d4339d67e7c34a6dc5cc37c39
Mzdora <35240166+mzdora@users.noreply.github.com>
2019-10-05 20:40:29 +0200

    Find the maximum and minimum value.and remove them

--- BST/BST.java
+++ BST/BST.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BST<E>
 {
     public class Node(){
@@ -75,9 +79,132 @@ public class BST<E>
         pretraverse(node.right);
     }
 
+    //前序遍历 深度优先遍历非递归写法
+    public void deepthtraverse(){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            System.out.println(node.e);
+            if(node.right != null){
+                stack.push(node.right);
+            }
+            if(node.left != null){
+                stack.push(node.left);
+            }
+        }
+    }
+
     //中序遍历，从左->右遍历。Node放中间。
-    //是二分搜索树里数字从小到大排列。
+    //中序遍历是二分搜索树里数字从小到大排列。
     //二分搜索树也叫排序树
+    public void midtraverse(){
+        midtraverse(root);
+    }
+
+    private void midtraverse(Node node){
+        if(node == null){
+            return;
+        }
+
+        midtraverse(node.left);
+        System.out.println(node.e);
+        midtraverse(node.right);
+    }
 
     //后续遍历，为二分搜索树释放内存
+    public void posttraverse(){
+        posttraverse(root);
+    }
+
+    private void posttraverse(Node node){
+        if(node == null){
+            return;
+        }
+
+        posttraverse(node.left);
+        posttraverse(node.right);
+        System.out.println(node.e);
+    }
+
+    //广度优先遍历，只能用非递归实现
+    public void weightraverse(){
+        //Queue只是一个接口，需要用LinkedList来Implement.
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node node = q.remove();
+            System.out.println(node.e);
+            if(node.left != null)
+                q.add(node.right);
+            if(node.right != null)
+                q.add(node.left);
+        }
+    }
+
+    //找到树中的最小值
+    public E getMin(){
+        if(size == 0){
+            throw new IllegalArgumentException("BST is empty");
+        }
+        E res = getMin(root).e;
+        return res;
+    }
+
+    private Node getMin(Node node){
+        if(node.left == null){
+            return node;
+        }
+        getMin(node.left);
+    }
+
+    //找到树中的最大值
+    public E getMax(){
+        if(size == 0){
+            throw new IllegalArgumentException("BST is empty");
+        }
+        E res = getMin(root).e;
+        return res;
+    }
+
+    private Node getMax(Node node){
+        if(node.right == null){
+            return node;
+        }
+        getMin(node.right);
+    }
+
+    //删除树中的最小值
+    public E removemin(){
+        removemin(root);
+        return getMin();
+    }
+
+    private Node removemin(Node node){
+        if(node.left == null){
+            Node res = node.right;
+            node.right = null;
+            Size--;
+            return res;
+        }
+
+        removemin(node.left);
+    }
+
+    //删除树中的最大值
+    public E removemax(){
+        removemax(root);
+        return getMax();
+    }
+
+    private Node removemax(Node node){
+        if(node.right == null){
+            Node res = node.left;
+            node.left = null;
+            Size--;
+            return res;
+        }
+
+        removemin(node.right);
+    }
 }
