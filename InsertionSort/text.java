a169146cfe86c172151d8dce9897324da63ec073
Mzdora <35240166+mzdora@users.noreply.github.com>
2020-02-18 15:11:06 +0100

    Update InsertionSort
    DataStructure I first time Lecture.
    Sorting is so fun <3

---
+++ InsertionSort/text.java
@@ -0,0 +1,9 @@
+public class text{
+    public static void main(String[] args){
+        int[] arr = {5,5,6,7,4,2,1,1995 };
+        sort.InsertionSort(arr);
+        for(int i = 0;i<arr.length;i++){
+            System.out.println( arr[i] );
+        }
+    }
+}
